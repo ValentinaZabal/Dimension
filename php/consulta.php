0eed3630b451383596afacc4c29f1de7b0eb8733
fsuaya <106682693+fsuaya@users.noreply.github.com>
2022-06-23 14:17:36 -0300

    Add files via upload

---
+++ php/consulta.php
@@ -0,0 +1,30 @@
+<?php
+
+include 'conexion_be.php';
+
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$correo = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+
+$query = "INSERT INTO contacto (nombre, apellido, correo, mensaje)
+        VALUES('$nombre','$apellido','$correo','$mensaje')";
+
+
+$ejecutar= mysqli_query($conexion, $query);
+if($ejecutar) {
+    echo '
+    <script>
+        alert("consulta enviada con exito");
+        window.location= "../index.php";
+    </script>
+    ';
+} else { '
+    <script>
+        alert("error al generar la consulta");
+        window.location= "../index.php";
+    </script>
+    ';
+}
+mysqli_close($conexion);
+?>
